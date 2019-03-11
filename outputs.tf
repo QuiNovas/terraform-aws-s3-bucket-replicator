640e876a0f92c788ab96ed95412e8ba7612c7773
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-03-11 16:24:19 -0500

    updated output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "kms_key_arn" {
   description = "The arn of the KMS key used to encrypt the environment variables"
-  value       = "${var.kms_key_arn == "" ? aws_kms_key.lambda_environment_variables.arn : var.kms_key_arn}"
+  value       = "${var.kms_key_arn == "" ? aws_kms_key.lambda_environment_variables.*.arn[0] : var.kms_key_arn}"
 }
 
 output "replicator_arn" {
