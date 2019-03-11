a41840020da0d8c667a57480d7137515e45f506b
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-03-11 16:12:55 -0500

    reodering

--- variables.tf
+++ variables.tf
@@ -3,13 +3,13 @@ variable "dead_letter_arn" {
   type        = "string"
 }
 
-variable "destination_bucket_obj_arns" {
-  description = "A list of bucket arns to replicate to. Must be in the format arn:aws:s3:::my_bucket/*"
+variable "destination_bucket_ids" {
+  description = "A list of bucket name/ids to replicate to."
   type        = "list"
 }
 
-variable "destination_bucket_ids" {
-  description = "A list of bucket name/ids to replicate to."
+variable "destination_bucket_obj_arns" {
+  description = "A list of bucket arns to replicate to. Must be in the format arn:aws:s3:::my_bucket/*"
   type        = "list"
 }
 
