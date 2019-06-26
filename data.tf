870c177f1243fa029dd253031130bbc8f8f3e9cf
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-06-26 16:27:04 +0530

    tf 12 upgrade

--- data.tf
+++ data.tf
@@ -1,25 +1,24 @@
 data "aws_s3_bucket" "source" {
-  bucket = "${var.source_bucket}"
+  bucket = var.source_bucket
 }
 
 data "aws_iam_policy_document" "replicator" {
   statement {
-    actions   = [
-      "s3:GetObject*"
+    actions = [
+      "s3:GetObject*",
     ]
     resources = [
-      "${data.aws_s3_bucket.source.arn}/*"
+      "${data.aws_s3_bucket.source.arn}/*",
     ]
-    sid       = "AllowCopyingFrom"
+    sid = "AllowCopyingFrom"
   }
   statement {
-    actions   = [
+    actions = [
       "s3:DeleteObject*",
-      "s3:PutObject*"
+      "s3:PutObject*",
     ]
-    resources = [
-      "${var.destination_bucket_obj_arns}"
-    ]
+    resources = var.destination_bucket_obj_arns
     sid       = "AllowCopyingTo"
   }
 }
+
--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 resource "aws_iam_policy" "replicator" {
-  name    = "LambdaLambdaLambdaReplicator"
-  policy  = "${data.aws_iam_policy_document.replicator.json}"
+  name   = "LambdaLambdaLambdaReplicator"
+  policy = data.aws_iam_policy_document.replicator.json
 }
 
 module "replicator" {
-  dead_letter_arn   = "${var.dead_letter_arn}"
-  environment_variables {
-    DESTINATION_BUCKETS = "${join(",", var.destination_bucket_ids)}"
+  dead_letter_arn = var.dead_letter_arn
+  environment_variables = {
+    DESTINATION_BUCKETS = join(",", var.destination_bucket_ids)
   }
-  handler           = "function.handler"
-  kms_key_arn       = "${var.kms_key_arn}"
-  name              = "s3-bucket-replicator"
-  policy_arns       = [
-    "${aws_iam_policy.replicator.arn}",
+  handler     = "function.handler"
+  kms_key_arn = var.kms_key_arn
+  name        = "s3-bucket-replicator"
+  policy_arns = [
+    aws_iam_policy.replicator.arn,
   ]
   policy_arns_count = 1
   runtime           = "python3.7"
@@ -25,21 +25,20 @@ module "replicator" {
 resource "aws_lambda_permission" "allow_bucket_execution" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
-  function_name = "${module.replicator.arn}"
+  function_name = module.replicator.arn
   principal     = "s3.amazonaws.com"
-  source_arn    = "${data.aws_s3_bucket.source.arn}"
+  source_arn    = data.aws_s3_bucket.source.arn
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = "${data.aws_s3_bucket.source.id}"
-  depends_on = [
-    "aws_lambda_permission.allow_bucket_execution",
-  ]
+  bucket     = data.aws_s3_bucket.source.id
+  depends_on = [aws_lambda_permission.allow_bucket_execution]
   lambda_function {
-    lambda_function_arn = "${module.replicator.arn}"
-    events              = [
+    lambda_function_arn = module.replicator.arn
+    events = [
       "s3:ObjectCreated:*",
       "s3:ObjectRemoved:*",
     ]
   }
 }
+
